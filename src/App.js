fefef3be9399055b0cc4bb6ecd4d1fad8da255a8
adriel_pesi <texte.0072002@gmail.com>
2024-03-23 17:39:25 -0300

    inicialização de projeto pessoal

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from './componetes/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+          teste
+          </li>
+        </ul>
+      </div>
+      <div> 
+        <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componetes/Header/index.js
@@ -0,0 +1,15 @@
+import './estilo.css'
+
+function Header() {
+    return(
+        <div className='teste   '>
+            <ul>
+                <li>
+                    teste
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
